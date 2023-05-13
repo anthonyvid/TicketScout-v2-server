6ee2c6669f07f63b8ad14980bf5db7690632b347
tamvoesdev2 <dev2@tamvoes.com>
2023-05-13 19:53:30 -0400

    .

--- models/Organization.js
+++ models/Organization.js
@@ -29,7 +29,7 @@ const OrganizationSchema = new mongoose.Schema(
 			type: Number,
 			required: true,
 		},
-		organization: {
+		settings: {
 			type: Object,
 			default: {
 				ticketStatuses: defaultTicketStatuses,
